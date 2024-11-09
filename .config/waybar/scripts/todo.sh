dcbb5393ddcd9b961708cd79249e1ce5d0a2562e
akcaymd <furkanakcay227@gmail.com>
2024-11-09 16:15:25 +0300

    todotxt added

---
+++ .config/waybar/scripts/todo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source: https://www.pedaldrivenprogramming.com/2023/09/simple-todo-app-waybar-hyprland/
+COUNT=$(wc -l < /mnt/Data/IMF/todo.todotxt)
+TODOS=$(cat ~/todo.txt | head -c -1 - | sed -z 's/\n/\\n/g')
+printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
